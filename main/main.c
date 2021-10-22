787c9df0f318eefd74fa2d6e52da9578c487ba64
Shivang Chikani <shivangchikani1@gmail.com>
2021-10-22 15:17:09 +0530

    ota_advanced

--- main/main.c
+++ main/main.c
@@ -13,10 +13,8 @@
 #include <driver/gpio.h>
 #include <protocol_examples_common.h>
 
-
 #define TAG "OTA"
 xSemaphoreHandle ota_semaphore;
-
 
 extern const uint8_t server_cert_pem_start[] asm("_binary_google_cer_start");
 
@@ -25,33 +23,102 @@ esp_err_t client_event_handler(esp_http_client_event_t *evt)
     return ESP_OK;
 }
 
+esp_err_t validate_image_header(esp_app_desc_t *incoming_ota_desc)
+{
+    const esp_partition_t *running_partition = esp_ota_get_running_partition();
+    esp_app_desc_t running_partition_description;
+    esp_ota_get_partition_description(running_partition, &running_partition_description);
+
+    printf("Current version is %s\n", running_partition_description.version);
+    printf("New version is %s\n", incoming_ota_desc->version);
+
+    if (strcmp(running_partition_description.version, incoming_ota_desc->version) == 0)
+    {
+        printf("New Version is same as the current version. Aborting\n");
+        return ESP_FAIL;
+    }
+    return ESP_OK;
+}
+
 void run_ota(void *params)
 {
+    ESP_ERROR_CHECK(nvs_flash_init());
+    esp_netif_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
     while (true)
     {
-        if (xSemaphoreTake(ota_semaphore, portMAX_DELAY))
+        xSemaphoreTake(ota_semaphore, portMAX_DELAY);
+
+        ESP_LOGI(TAG, "Invoking OTA");
+        ESP_ERROR_CHECK(example_connect());
+
+        esp_http_client_config_t clientConfig = {
+            .url = "https://drive.google.com/u/1/uc?id=1cRs3hP7sODPdl6OeOkjW1LIOVEz4FdQ4&export=download", // our ota location
+            .event_handler = client_event_handler,
+            .cert_pem = (char *)server_cert_pem_start};
+
+        esp_https_ota_config_t ota_config = {
+            .http_config = &clientConfig};
+
+        esp_https_ota_handle_t ota_handle = NULL;
+
+        if (esp_https_ota_begin(&ota_config, &ota_handle) != ESP_OK)
         {
-            ESP_LOGI(TAG, "Invoking OTA");
-
-            ESP_ERROR_CHECK(nvs_flash_init());
-            esp_netif_init();
-            ESP_ERROR_CHECK(esp_event_loop_create_default());
-            ESP_ERROR_CHECK(example_connect());
-
-            esp_http_client_config_t clientConfig = {
-                .url = "https://drive.google.com/u/1/uc?id=1cRs3hP7sODPdl6OeOkjW1LIOVEz4FdQ4&export=download", // our ota location
-                .event_handler = client_event_handler,
-                .cert_pem = (char *)server_cert_pem_start};
-
-            if (esp_https_ota(&clientConfig) == ESP_OK)
-            {
-                printf("Restarting in 5 seconds\n");
-                vTaskDelay(pdMS_TO_TICKS(5000));
-                esp_restart();
-            }
-            ESP_LOGE(TAG, "Failed to update firmware");
+            ESP_LOGE(TAG, "esp_https_ota_begin failed");
+            esp_https_ota_finish(ota_handle);
+            example_disconnect();
+            continue;
         }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        esp_app_desc_t incoming_ota_desc;
+
+        if (esp_https_ota_get_img_desc(ota_handle, &incoming_ota_desc) != ESP_OK)
+        {
+            ESP_LOGE(TAG, "esp_https_ota_get_img_desc failed");
+            esp_https_ota_finish(ota_handle);
+            example_disconnect();
+            continue;
+        }
+
+        if (validate_image_header(&incoming_ota_desc) != ESP_OK)
+        {
+            ESP_LOGE(TAG, "Validate_image_header failed");
+            esp_https_ota_finish(ota_handle);
+            example_disconnect();
+            continue;
+        }
+        while (true)
+        {
+            esp_err_t ota_result = esp_https_ota_perform(ota_handle);
+            if (ota_result != ESP_ERR_HTTPS_OTA_IN_PROGRESS)
+                break;
+        }
+
+        if (esp_https_ota_finish(ota_handle) != ESP_OK)
+        {
+            ESP_LOGE(TAG, "esp_https_ota_finish failed");
+            esp_https_ota_finish(ota_handle);
+            example_disconnect();
+            continue;
+        }
+        else
+        {
+            for (int i = 5; i > 0; --i)
+            {
+                printf("Restarting in %d seconds\n", i);
+                vTaskDelay(1000 / portTICK_PERIOD_MS);
+            }
+            esp_restart();
+        }
+
+        // if (esp_https_ota(&clientConfig) == ESP_OK)
+        // {
+        //     printf("Restarting in 5 seconds\n");
+        //     vTaskDelay(pdMS_TO_TICKS(5000));
+        //     esp_restart();
+        // }
+        ESP_LOGE(TAG, "Failed to update firmware");
     }
 }
 
